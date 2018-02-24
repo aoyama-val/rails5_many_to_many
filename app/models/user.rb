4002704f6828b3f0abeac298cf029bb1ce00a2ab
Aoyama, Shotaro <aoyama@val.co.jp>
2018-02-24 16:21:36 +0900

    Userテーブルを追加

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224070741) do
+ActiveRecord::Schema.define(version: 20180224071333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20180224070741) do
     t.string "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
